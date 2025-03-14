4ad6b37bbe7824415512844ecde78d9e1411fbac
Ella Åkesson Hummerdal <ella.belle.hummerdahl@gmail.com>
2025-03-14 11:31:01 +0100

    [Add] snyggt Rain

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Card = ({ card, cardSelected }) => {
   return <div className="card" onClick={() => cardSelected(card.id)}>
     
-    {card.name}
   <img src={card.show ? card.imageUrl : card.poster}/ >
   </div>;
 };
