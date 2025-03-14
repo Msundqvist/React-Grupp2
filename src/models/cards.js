e6155064fccbca1c646a4aebd503f47e854e1da5
MatildaSundqvist <matildasundqvist@live.se>
2025-03-14 09:59:06 +0100

    added js cards

---
+++ src/models/cards.js
@@ -0,0 +1,8 @@
+export class Cards {
+    constructor(id, name, poster, imgUrl) {
+        this.id = id,
+            this.name = name,
+            this.poster = poster,
+            this.imgUrl = imgUrl
+    }
+}
