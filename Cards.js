8aa4b7faa024c12a23b6c5310931e5b401d3fa51
MatildaSundqvist <matildasundqvist@live.se>
2025-03-14 10:31:46 +0100

    changed id

--- Cards.js
+++ Cards.js
@@ -1,83 +1,82 @@
-{
-    "Cards": [
+export const cards =
+    [
         {
             "id": 1,
             "name": "Aragorn",
             "imageUrl": "Aragorn.jpg",
             "poster": "Poster.jpg"
-           
-             
+
+
         },
         {
-            "id": 1,
+            "id": 2,
             "name": "Arwen",
             "imageUrl": "Arwen.jpg",
             "poster": "Poster.jpg"
         },
         {
-            "id": 1,
+            "id": 3,
             "name": "Beers",
             "imageUrl": "Beers.jpg",
             "poster": "Poster.jpg"
         },
         {
-            "id": 1,
+            "id": 4,
             "name": "Boromir",
             "imageUrl": "Boromir.jpg",
             "poster": "Poster.jpg"
         },
         {
-            "id": 1,
+            "id": 5,
             "name": "FrodoGandalf",
             "imageUrl": "FrodoGandalf.jpg",
             "poster": "Poster.jpg"
 
         },
         {
-            "id": 1,
+            "id": 6,
             "name": "Galadriel",
             "imageUrl": "Galadriel.jpg",
             "poster": "Poster.jpg"
         },
         {
-            "id": 1,
+            "id": 7,
             "name": "Gimli",
             "imageUrl": "Gimli.jpg",
             "poster": "Poster.jpg"
         },
         {
-            "id": 1,
+            "id": 8,
             "name": "Gollum",
             "imageUrl": "Gollum.jpg",
             "poster": "Poster.jpg"
         },
         {
-            "id": 1,
+            "id": 9,
             "name": "Kuppa",
             "imageUrl": "Kuppa.jpg",
             "poster": "Poster.jpg"
         },
         {
-            "id": 1,
+            "id": 10,
             "name": "Legolas",
             "imageUrl": "Legolas.jpg",
             "poster": "Poster.jpg"
         },
         {
-            "id": 1,
+            "id": 11,
             "name": "Pippin",
             "imageUrl": "Pippin.jpg",
             "poster": "Poster.jpg"
         },
         {
-            "id": 1,
+            "id": 12,
             "name": "Samwise",
             "imageUrl": "Samwise.jpg",
             "poster": "Poster.jpg"
         },
-      
-        
-        
+
+
+
 
     ]
-}
