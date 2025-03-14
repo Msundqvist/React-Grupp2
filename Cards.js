0ec80151a246a90c1479ca3d629b0e18e97614ca
Ella Åkesson Hummerdal <ella.belle.hummerdahl@gmail.com>
2025-03-14 10:52:08 +0100

    [Add] images to public and fix image tagg

--- Cards.js
+++ Cards.js
@@ -3,77 +3,77 @@ export const cards =
         {
             "id": 1,
             "name": "Aragorn",
-            "imageUrl": "Aragorn.jpg",
-            "poster": "Poster.jpg"
+            "imageUrl": "/images/Aragorn.jpg",
+            "poster": "/images/Poster.jpg"
 
 
         },
         {
             "id": 2,
             "name": "Arwen",
-            "imageUrl": "Arwen.jpg",
-            "poster": "Poster.jpg"
+            "imageUrl": "/images/Arwen.jpg",
+            "poster": "/images/Poster.jpg"
         },
         {
             "id": 3,
             "name": "Beers",
-            "imageUrl": "Beers.jpg",
-            "poster": "Poster.jpg"
+            "imageUrl": "/images/Beers.jpg",
+            "poster": "/images/Poster.jpg"
         },
         {
             "id": 4,
             "name": "Boromir",
-            "imageUrl": "Boromir.jpg",
-            "poster": "Poster.jpg"
+            "imageUrl": "/images/Boromir.jpg",
+            "poster": "/images/Poster.jpg"
         },
         {
             "id": 5,
             "name": "FrodoGandalf",
-            "imageUrl": "FrodoGandalf.jpg",
-            "poster": "Poster.jpg"
+            "imageUrl": "/images/FrodoGandalf.jpg",
+            "poster": "/images/Poster.jpg"
 
         },
         {
             "id": 6,
             "name": "Galadriel",
-            "imageUrl": "Galadriel.jpg",
-            "poster": "Poster.jpg"
+            "imageUrl": "/images/Galadriel.jpg",
+            "poster": "/images/Poster.jpg"
         },
         {
             "id": 7,
             "name": "Gimli",
-            "imageUrl": "Gimli.jpg",
-            "poster": "Poster.jpg"
+            "imageUrl": "/images/Gimli.jpg",
+            "poster": "/images/Poster.jpg"
         },
         {
             "id": 8,
             "name": "Gollum",
-            "imageUrl": "Gollum.jpg",
-            "poster": "Poster.jpg"
+            "imageUrl": "/images/Gollum.jpg",
+            "poster": "/images/Poster.jpg"
         },
         {
             "id": 9,
             "name": "Kuppa",
-            "imageUrl": "Kuppa.jpg",
-            "poster": "Poster.jpg"
+            "imageUrl": "/images/Kuppa.jpg",
+            "poster": "/images/Poster.jpg"
         },
         {
             "id": 10,
             "name": "Legolas",
-            "imageUrl": "Legolas.jpg",
-            "poster": "Poster.jpg"
+            "imageUrl": "/images/Legolas.jpg",
+            "poster": "/images/Poster.jpg"
         },
         {
             "id": 11,
             "name": "Pippin",
-            "imageUrl": "Pippin.jpg",
-            "poster": "Poster.jpg"
+            "imageUrl": "/images/Pippin.jpg",
+            "poster": "/images/Poster.jpg"
         },
         {
             "id": 12,
             "name": "Samwise",
-            "imageUrl": "Samwise.jpg",
-            "poster": "Poster.jpg"
+            "imageUrl": "/images/Samwise.jpg",
+            "poster": "/images/Poster.jpg"
         },
 
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Card = ({ card }) => {
-  return <div>{card.name}</div>;
+  return <div>{card.name}
+  <img src={card.imageUrl}/ >
+  </div>;
 };
 
 export default Card;
